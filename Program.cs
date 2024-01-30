f01dab013a2afc085cb0f9f2615f14aaf977535b
AxelLofberg <axellofbergj@gmail.com>
2024-01-30 11:05:29 +0100

    testing branch

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Hello World!!!");
+
+app.MapGet("//", () => "Hej jag mår bra!");
 
 app.Run();
